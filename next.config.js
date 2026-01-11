4eb006689a3d91b71c12b8cb0cd2f9f2d208f7e6
careerschool-18 <careerschoolhr18@gmail.com>
2026-01-11 20:22:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'http://localhost:8080/api/v1/:path*', // backend URL
+        destination: 'https://careerschool.co.in/api/v1/:path*',
       },
     ]
   },
